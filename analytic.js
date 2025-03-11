db69fb264b90280c2348ead4dabad7e5c4a2d5e2
HouseJapri <fjapri@twilio.com>
2025-03-11 14:00:06 +0800

    enhance product tracking with detailed analytics on view and add to cart actions

--- analytic.js
+++ analytic.js
@@ -1,8 +1,9 @@
 const addToCartButton = document.querySelector("#add_to_cart");
+const productId = new URLSearchParams(window.location.search).get("id");
+const product = productDatabase[productId];
+
 addToCartButton.addEventListener("click", () => {
-  const productId = new URLSearchParams(window.location.search).get("id");
-  const product = productDatabase[productId];
-  analytics.track("clicked_add_to_cart", {
+  analytics.track("Add To Cart", {
     name: product.name,
     petCategory: product.petCategory,
     productCategory: product.productCategory,
@@ -20,4 +21,42 @@ addToCartButton.addEventListener("click", () => {
   });
   const cartCount = document.querySelector(".cart-count");
   analytics.identify({ cart_count: parseInt(cartCount.textContent) + 1 });
+});
+
+analytics.track("Product Viewed", {
+    name: product.name,
+    petCategory: product.petCategory,
+    productCategory: product.productCategory,
+    categoryPath: product.categoryPath,
+    originalPrice: product.originalPrice,
+    currentPrice: product.currentPrice,
+    discount: product.discount,
+    description: product.description,
+    sku: product.sku,
+    category: product.category,
+    tags: product.tags,
+    rating: product.rating,
+    mainImage: product.mainImage,
+    features: product.features,
+    });
+
+const viewProductButtons = document.querySelectorAll("#productViewedButton");
+viewProductButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const href = button.getAttribute("href");
+    const productId = href.split("?id=")[1];
+    const product = productDatabase[productId];
+    analytics.track("Click View Product", {
+      name: product.name,
+      petCategory: product.petCategory,
+      productCategory: product.productCategory,
+      categoryPath: product.categoryPath,
+      originalPrice: product.originalPrice,
+      currentPrice: product.currentPrice,
+      discount: product.discount,
+      description: product.description,
+      sku: product.sku,
+      tags: product.tags,     
+    });
+  });
 });
