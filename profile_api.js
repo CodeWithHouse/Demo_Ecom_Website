e493db4207780b8a691b537a477c7dd3c9faaede
CodeWithHouse <firdhousejapri@gmail.com>
2025-03-11 16:40:50 +0800

    update profile API to conditionally append userID and anonymousID parameters

--- profile_api.js
+++ profile_api.js
@@ -3,7 +3,13 @@ async function getUserProfile() {
     const userID = analytics.user().id();
     const anonymousID = analytics.user().anonymousId();
     let endpoint = 'https://backend-ecom-demo.firdhousejapri.workers.dev/';
-    const param = new URLSearchParams({ userID, anonymousID });
+    const param = new URLSearchParams();
+    if (userID) {
+        param.append('userID', userID);
+    }
+    if (anonymousID) {
+        param.append('anonymousID', anonymousID);
+    }
     endpoint += `?${param.toString()}`;
     const response = await fetch(endpoint);
     if (response.ok) {
