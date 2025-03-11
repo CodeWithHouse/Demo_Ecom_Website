3bd7e8ed7df14903442a24fd746a91d628d6601e
HouseJapri <fjapri@twilio.com>
2025-03-11 13:03:00 +0800

    enhance add to cart tracking with product details

--- analytic.js
+++ analytic.js
@@ -1,6 +1,8 @@
-const addToCartButton = document.querySelector('#add_to_cart');
-addToCartButton.addEventListener('click', () => {
-  analytics.track('clicked_add_to_cart');
-  const cartCount = document.querySelector('.cart-count');
+const addToCartButton = document.querySelector("#add_to_cart");
+addToCartButton.addEventListener("click", () => {
+  const productId = new URLSearchParams(window.location.search).get("id");
+  const product = productDatabase[productId];
+  analytics.track("clicked_add_to_cart", product);
+  const cartCount = document.querySelector(".cart-count");
   analytics.identify({ cart_count: parseInt(cartCount.textContent) + 1 });
 });
