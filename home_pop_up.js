2fd2d84701274d6f7a41921dbb1c652f59c2fc4c
CodeWithHouse <firdhousejapri@gmail.com>
2025-03-11 16:55:12 +0800

    add personalized messages to popup based on user's most frequent tags

--- home_pop_up.js
+++ home_pop_up.js
@@ -9,12 +9,35 @@ async function waitForAnalytic (){
     });
 }
 
+const messages = {
+    "Nutrition": "We have the best nutrition for your pet",
+    "Toys": "Your pet will love our toys",
+    "Dog": "We have everything for your dog",
+    "Cat": "We have everything for your cat",
+}
+
 
 async function renderPopUP() {
     await waitForAnalytic();
     const profile = await getUserProfile();
     if (profile) {
-       console.log (profile);
+        const most_frequent_tags = profile.traits.most_frequent_tags;
+    if (most_frequent_tags) {
+        const message = messages[most_frequent_tags];
+        if (message) {
+            const popup = document.createElement("div");
+            popup.classList.add("popup");
+            popup.innerHTML = `
+                <div class="popup-content">
+                    <span class="close">&times;</span>
+                    <p>${message}</p>
+                </div>
+            `;
+            document.body.appendChild(popup);
+            document.querySelector(".close").addEventListener("click", () => {
+                popup.style.display = "none";
+            });
+        }}
     }
 }
 renderPopUP();
