2d88fdd7ec59aee052b5cf09f4b2cfdb010ba81f
HouseJapri <fjapri@twilio.com>
2025-03-11 12:55:35 +0800

    increment cart count on add to cart button click

--- analytic.js
+++ analytic.js
@@ -2,5 +2,5 @@ const addToCartButton = document.querySelector('#add_to_cart');
 addToCartButton.addEventListener('click', () => {
   analytics.track('clicked_add_to_cart');
   const cartCount = document.querySelector('.cart-count');
-  analytics.identify({ cart_count: cartCount.textContent });
+  analytics.identify({ cart_count: parseInt(cartCount.textContent) + 1 });
 });
